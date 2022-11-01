727171630f1d4a85813253c05907c75075b1b87a
William Flores <willyamdan10@gmail.com>
2022-11-01 11:32:47 -0600

    Adding functionality to the input form

--- perncrud/src/components/TaskForm.js
+++ perncrud/src/components/TaskForm.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
+import { useState, useEffect, React } from 'react'
 
 export default function Taskform() {
+
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+  }
+
+  const handleChange = (e) => setTask({ ...task, [e.target.name]: e.target.value });
+
+
   return (
-    <div>Taskform</div>
+    <Grid container direction='column' alignItems='center' justifyContent='center'>
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{
+            backgroundColor: '#1e272e',
+            padding: '1rm'
+          }}
+        >
+          <Typography variant='5' textAlign='center' color='white'>
+            Create Task
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant='filled'
+                label='Write Title'
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                name='title'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />
+              <TextField
+                variant='filled'
+                label='Write Description'
+                multiline
+                rows={4}
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                name='description'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />
+              <Button variant='contained' color='primary' type='submit'>
+                Save
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
